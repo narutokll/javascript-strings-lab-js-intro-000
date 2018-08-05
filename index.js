ffc21e2a90e3cbb5e464c7b02c446e2e6f188dbd
Preston K Chiew <johnjew90@yahoo.com>
2018-08-05 19:32:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
+<<<<<<< HEAD
 var greetSpecialGuest = 'Hello', +specialGuest+ '!';
+=======
+var greetSpecialGuest = "Hello",${specialGuest}!`;
+>>>>>>> 052a5ca436298db6f772a9191018ce9d41731908
 
 var topic = "space";
 
